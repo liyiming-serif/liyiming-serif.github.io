b831ae5a8905e34dab1f270133f08de128200531
liyiming-serif <liyiming.serif@gmail.com>
2017-11-02 04:10:15 -0400

    Update and rename get_level.js to get_params.js

---
+++ get_params.js
@@ -0,0 +1,33 @@
+//author: Virendra
+//source: http://www.jquerybyexample.net/2012/06/get-url-parameters-using-jquery.html
+
+function isNormalized(){
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == "normalized") 
+        {
+            if(sParameterName[1]=="true"){
+                return true;
+            }
+            return false;
+        }
+    }
+    return false;
+}
+
+function getLevel(){
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == "level") 
+        {
+            return parseInt(sParameterName[1]);
+        }
+    }
+    return 0;
+}
