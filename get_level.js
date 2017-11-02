5f8535e61a427e7404c42f6941a02c079d4c9ac8
liyiming-serif <liyiming.serif@gmail.com>
2017-11-02 03:54:07 -0400

    mm

---
+++ get_level.js
@@ -0,0 +1,14 @@
+function getLevel(){
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == "level") 
+        {
+            return parseInt(sParameterName[1]);
+        }
+    }
+    return 0;
+}​
