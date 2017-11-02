1f51347c2116d960c66451aaa40fce172f4348a0
liyiming-serif <liyiming.serif@gmail.com>
2017-11-01 23:56:38 -0400

    Update heatmap.js

--- heatmap.js
+++ heatmap.js
@@ -3,7 +3,64 @@ function create_heatmap(mouse_press, mouse_release, tower_create, svg) {
 }
 
 function map_tower_temporal(tower_data, level, svg){
+    //game dimensions
+    const GAME_WIDTH = 960;
+    const GAME_HEIGHT = 720;
+    const timestep = ; //time in seconds
+    
+    //graph dimensions
+    var grid_size = 16;
+    svg.attr("width", buckets[0].length*grid_size)
+        .attr("height", buckets.length*grid_size);
+    var colors = ['#ffffe5','#f7fcb9','#d9f0a3','#addd8e','#78c679','#41ab5d','#238443','#006837','#004529'];
+    
+    //bucketize data: initialize 2d spatial bucket
+    var buckets = new Array(Math.floor(GAME_HEIGHT/snap));
+    for (var i = 0; i < buckets.length; i++){
+        buckets[i] = new Array(Math.floor(GAME_WIDTH/snap));
+        for(var j = 0; j < buckets[i].length; j++){
+            buckets[i][j] = 0;
+        }
+    }
+    var max_bucket_val = 0;
+    //fill buckets with mouse_release data
+    for (var i = 0; i<mouse_data.length; i++){
+        if(mouse_data[i]["Level"] == level){
+            var x = Math.floor(mouse_data[i]["X"]/snap);
+            var y = Math.floor(mouse_data[i]["Y"]/snap);
+            if(y>=buckets.length){
+                y = buckets.length-1;
+            }
+            if(x>=buckets[y].length){
+                x = buckets[y].length-1;
+            }
 
+            buckets[y][x] += 1; 
+            if(buckets[y][x] > max_bucket_val){
+                max_bucket_val = buckets[y][x];
+            }
+        }
+    }
+
+
+
+    //create grid squares
+    var colorScale = d3.scale.quantile()
+        .domain([0, colors.length-1, max_bucket_val])
+        .range(colors);
+
+    for(var y = 0; y < buckets.length; y++){
+        for(var x = 0; x < buckets[y].length; x++){
+            svg.append("rect")
+                .attr("x", x*grid_size)
+                .attr("y", y*grid_size)
+                .attr("width", grid_size)
+                .attr("height", grid_size)
+                .attr("rx", 4)
+                .attr("ry", 4)
+                .attr("fill", colorScale(buckets[y][x]));
+        }
+    }
 }
 
 function map_mouse_tutorial_clicks(mouse_data, level, svg){
@@ -65,7 +122,7 @@ function map_mouse_tutorial_clicks(mouse_data, level, svg){
     }
 }
 
-function map_tower_spatial(tower_data, level, svg, includeMat){
+function map_tower_spatial(tower_data, level, svg, normalizeHeight){
     //game dimensions
     const GAME_WIDTH = 960;
     const GAME_HEIGHT = 720;
@@ -91,7 +148,7 @@ function map_tower_spatial(tower_data, level, svg, includeMat){
             x = buckets[y].length-1;
         }
 
-        buckets[y][x] += includeMat ? tower_data[i]["Materials"] : 1; 
+        buckets[y][x] += normalizeHeight ? tower_data[i]["Materials"] : 1; 
         if(buckets[y][x] > max_bucket_val){
             max_bucket_val = buckets[y][x];
         }
