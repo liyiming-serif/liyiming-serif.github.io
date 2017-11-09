4510b5b0e0f9aaf88ef29c1e6094090c13e215be
liyiming-serif <liyiming.serif@gmail.com>
2017-11-09 15:34:53 -0500

    mm

---
+++ piechart.js
@@ -0,0 +1,23 @@
+//@author: Yiming Li
+
+function create_piechart(data, svg) { 
+    var colors = ['#8dd3c7','#ffffb3','#bebada'];
+    var colorScale = d3.scaleOrdinal()
+        .range(colors);
+    svg.append('g')
+    	.attr('transform', 'translate(' + (width / 2) +  ',' + (height / 2) + ')');
+
+    var radius = Math.min(svg.style("width"),svg.style("height"))/2;
+    var arc = d3.arc()
+		.innerRadius(0)
+		.outerRadius(radius);
+	var pie = d3.pie()
+		.value(function(d){return d.count; })
+		.sort(null);
+	var path = svg.selectAll('path')
+		.data(pie(data))
+		.enter()
+		.append('path')
+		.attr('d', arc)
+		.attr('fill', function(d,i){return colorScale(d.data.label); });
+}
---
+++ piechart.parser.js
@@ -0,0 +1,33 @@
+function parsePie(data){
+    //build the dictionary of game overs
+    var losses = {};
+    var actions = JSON.parse(data)["player_actions"];
+    for(var i = 0; i < actions.length; i++){
+        if(actions[i]["action_id"] == 6){ //hit a game over!
+            losses[actions[i]["dynamic quest id"]] = actions[i]["log_timestamp"];
+        }
+    }
+
+
+    //loop through level loads
+    var output = [];
+    var wins = 0;
+    var losses = 0;
+    var quits = 0;
+    var loads = JSON.parse(data)["player_quests"];
+    for(var i = 0; i < loads.length; i++){
+        if(loads[i]["end_timestamp"] == null){ //player quit
+            quits += 1;
+        }
+        output.push({"name":"Quits", "count":quits});
+        else if(losses.hasOwnProperty(loads[i]["dynamic_quest_id"])){ //player lost
+            losses += 1;
+        }
+        output.push({"name":"Losses", "count":losses});
+        else{ //player won
+            wins += 1;
+        }
+        output.push({"name":"Wins", "count":wins});
+    }
+    return output;
+};
