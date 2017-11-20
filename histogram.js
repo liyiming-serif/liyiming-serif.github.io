0e6c610267a4d11d9ceec821eb070ee17c0fe359
liyiming-serif <liyiming.serif@gmail.com>
2017-11-20 03:37:40 -0500

    Update histogram.js

--- histogram.js
+++ histogram.js
@@ -31,11 +31,11 @@ function create_histogram(data, svg) {
     var xAxis = d3.axisBottom(xScale).tickValues(ticks)
         .tickFormat(d3.format("1"));       
     svg.append("g")
-       .attr("transform", "translate(35, 520)").call(xAxis);
+       .attr("transform", "translate(0, 520)").call(xAxis);
             
     var yAxis = d3.axisLeft(yScale);
     svg.append("g")
-       .attr("transform", "translate(80,0)").call(yAxis);
+       .attr("transform", "translate(45,0)").call(yAxis);
 
     var margin = {top: 20, right: 20, bottom: 50, left: 70};
     svg.append("text")
